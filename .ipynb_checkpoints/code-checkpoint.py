2e379c9ccdf4149824e1dba026b0989c98f00044
rjthescholar <rjthescholar@gmail.com>
2020-09-22 15:29:45 -0400

    Added functionality to code.py

---
+++ .ipynb_checkpoints/code-checkpoint.py
@@ -0,0 +1,10 @@
+## The simple code
+
+
+input1 = input('Enter your name!')
+print("Your name is " + input1)
+
+input2 = input("Enter your game!")
+print("Your game is " + input2)
+
+print("Yor name is cool, " + input1 + ", and your game called " + input2 +" has rules, but don't you forget this ditty!")
--- code.py
+++ code.py
@@ -1,5 +1,10 @@
 ## The simple code
 
 
-input1 = input('Enter the first word you would like to concatenate');
-print(input1);
+input1 = input('Enter your name!\n')
+print("Your name is " + input1)
+
+input2 = input("\nEnter your game!\n")
+print("Your game is " + input2)
+
+print("\nYour name is cool, " + input1 + ", and your game called " + input2 +" has rules, but don't you forget this ditty!")

796cbad5dffa37a5c0d74e5f904eeab9b6900feb
jsl74 <71395249+jsl74@users.noreply.github.com>
2020-09-22 16:24:52 -0400

    Update code.py

--- code.py
+++ code.py
@@ -7,4 +7,8 @@ print("Your name is " + input1)
 input2 = input("\nEnter your game!\n")
 print("Your game is " + input2)
 
-print("\nYour name is cool, " + input1 + ", and your game called " + input2 +" has rules, but don't you forget this ditty!")
+input3 = input("\nEnter your favorite color!\n")
+print("Your favorite color is " + input3)
+
+print("\nYour name is cool, " + input1 + ". Your game called " + input2 +" has rules, but don't you forget this ditty!" + " I like " + input3 + ", too!")
+
